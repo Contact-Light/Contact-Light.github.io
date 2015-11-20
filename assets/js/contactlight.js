6d66b6bf7d8d3cb3fc78f257bac3310fb88158bd
jeremyblaze <contact@jeremyblaze.com>
2015-11-20 11:00:58 +1100

    Progress
    - Added in lots of content including the about page
    - Added some sexy fade-in effects

--- assets/js/contactlight.js
+++ assets/js/contactlight.js
@@ -1,7 +1,13 @@
-// Some code
+// Flyout menu
 
 	$(document).ready(function(){
 
-		
+		$('.menuToggle').click(function(){
+
+			$(this).toggleClass('menuVisible');
+			$('.site').toggleClass('menuVisible');
+			$('.flyout').toggleClass('menuVisible');
+
+		});
 
 	});
